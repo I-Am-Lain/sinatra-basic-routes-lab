cadbf58ce3bb7b9a570c10fe331bb2f826d4f262
I-Am-Lain <vetter.william@gmail.com>
2020-10-05 10:37:39 -0500

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,16 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+    get '/name' do
+        "My name is Sqwilliam."
+    end  
+
+    get '/hometown' do
+        "My hometown is Bikini Bottom."
+    end  
+
+    get '/favorite-song' do
+        "My favorite song is The Piano Concerto No. 5 in E♭ major, Op. 73, by Ludwig van Beethoven"
+    end  
 end
